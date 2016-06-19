cb5dbc90e25abca50fcc8718cf6d362cad11d5ee
Nuria <ngarciacarrillo@danielcastelao.com>
2016-06-20 00:12:58 +0200

    finalizado

--- src/proyectofacebook1/ProyectoFacebook1.java
+++ src/proyectofacebook1/ProyectoFacebook1.java
@@ -13,37 +13,40 @@ import javax.swing.JOptionPane;
  */
 public class ProyectoFacebook1 {
 
-   
     public static void main(String[] args) {
-    metodos metodosFace = new metodos();
-    metodosFace.conectar();
-    int opcion = Integer.parseInt(JOptionPane.showInputDialog("Elija que hacer:\n 1."
-            + " Ver usuario conectado \n 2. Publicar un estado \n 3.Buscar una persona\n"
-            + "4. Comentar una publicacion\n 5. Darle me gusta a una publicacion"));
+        metodos metodosFace = new metodos();
+        metodosFace.conectar();
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Elija que hacer:\n 1."
+                + " Ver usuario conectado \n 2. Publicar un estado \n 3.Buscar un tema\n"
+                + "4. Comentar una publicacion\n 5. Darle me gusta a una publicacion\n 6.Imprimir los comentarios"
+                + "de una publicacion\n 6. Postear una foto"));
 
-    switch (opcion) {
-                case 1:
-                    metodosFace.user();
-                    break;
-                case 2:
-                    metodosFace.publicar();
-                    break;
-                case 3:
-                    metodosFace.buscarPersonas();
-                    break;
-                case 4:
-                    metodosFace.comentarEstado();
-                    break;
-                case 5:
-                    metodosFace.likePost();
-                    break;
-                case 6:
-                    System.exit(0);
-              
-                    
-                            }
-    
-    
+        switch (opcion) {
+            case 1:
+                metodosFace.user();
+                break;
+            case 2:
+                metodosFace.publicar();
+                break;
+            case 3:
+                metodosFace.buscarTema();
+                break;
+            case 4:
+                metodosFace.comentarEstado();
+                break;
+            case 5:
+                metodosFace.likePost();
+                break;
+            case 6:
+                metodosFace.comentariosPost();
+                break;
+            case 7:
+                metodosFace.subirFoto();
+                break;
+            case 8:
+                System.exit(0);
+
+        }
+
     }
-    
 }
